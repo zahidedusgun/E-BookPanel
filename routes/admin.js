de8c56d5c69f3d71853cdaeb5f3e822735dc766c
zahidsgn <zahide.dusgun@gmail.com>
2024-02-08 02:42:35 +0300

    Image settings completed

--- routes/admin.js
+++ routes/admin.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const path = require("path");
+const fs = require("fs");
 
 const db = require("../data/db");
-
+const imageUpload = require("../helpers/image-upload");
 //Create a book
 router.get("/book/create", async (req, res) => {
   try {
@@ -19,46 +20,49 @@ router.get("/book/create", async (req, res) => {
 });
 
 //Creating a book
-router.post("/book/create", async (req, res) => {
-  const name = req.body.name;
-  const description = req.body.description;
-  const image = req.body.image;
-  const category = req.body.category;
-  const home = req.body.home == "on" ? 1 : 0;
-  const accept = req.body.accept == "on" ? 1 : 0;
+router.post(
+  "/book/create",
+  imageUpload.upload.single("image"),
+  async (req, res) => {
+    const name = req.body.name;
+    const description = req.body.description;
+    const image = req.file.filename;
+    const category = req.body.category;
+    const home = req.body.home == "on" ? 1 : 0;
+    const accept = req.body.accept == "on" ? 1 : 0;
 
-  try {
-    await db.execute(
-      "INSERT INTO books (name, description, image, categoryid, home, accept) VALUES (?, ?, ?, ?, ?, ?)",
-      [name, description, image, category, home, accept]
-    );
-     res.redirect("/admin/books?action=created");
-  } catch (err) {
-    console.log(err);
-    res.status(500).json({ error: "Internal Server Error" });
+    try {
+      await db.execute(
+        "INSERT INTO books (name, description, image, categoryid, home, accept) VALUES (?, ?, ?, ?, ?, ?)",
+        [name, description, image, category, home, accept]
+      );
+      res.redirect("/admin/books?action=created");
+    } catch (err) {
+      console.log(err);
+      res.status(500).json({ error: "burası" });
+    }
   }
-});
+);
 
 //Selecting a book
 router.get("/books/:bookId", async (req, res) => {
   const bookId = req.params.bookId;
   try {
-    const [books, ] = await db.execute("SELECT * FROM books WHERE bookId = ?", [
+    const [books] = await db.execute("SELECT * FROM books WHERE bookId = ?", [
       bookId,
     ]);
-    const [categories, ] = await db.execute("SELECT * FROM category");
+    const [categories] = await db.execute("SELECT * FROM category");
     const book = books[0];
     if (book) {
-      return res.render("admin/book-edit",
-        {
-          name: book.name,
-          description: book.description,
-          book: book,
-          image: book.image,
-          categories: categories,
-        });
+      return res.render("admin/book-edit", {
+        name: book.name,
+        description: book.description,
+        book: book,
+        image: book.image,
+        categories: categories,
+      });
     }
-    
+
     res.redirect("admin/blogs");
   } catch (err) {
     console.log(err);
@@ -67,31 +71,44 @@ router.get("/books/:bookId", async (req, res) => {
 });
 
 //Editing the book
-router.post("/books/:bookId", async (req, res) => {
-  const bookId = req.params.bookId;
-  const name = req.body.name;
-  const description = req.body.description;
-  const image = req.body.image;
-  const category = req.body.category;
+router.post(
+  "/books/:bookId",
+  imageUpload.upload.single("image"),
+  async (req, res) => {
+    const bookId = req.params.bookId;
+    const name = req.body.name;
+    const description = req.body.description;
+    let image = req.body.image;
+    const category = req.body.category;
 
-  try {
-    await db.execute(
-      "UPDATE books SET name = ?, description = ?, image = ?, categoryid = ? WHERE bookId = ?",
-      [name, description, image, category, bookId]
-    );
-    res.redirect("/admin/books?action=updated");
-  } catch (err) {
-    console.log(err);
-    res.status(500).json({ error: "Internal Server Error" });
+    if (req.file) {
+      image = req.file.filename;
+      fs.unlink("./public/images/" + req.body.image, (err) => {
+        if (err) {
+          console.log(err);
+        }
+      });
+    }
+
+    try {
+      await db.execute(
+        "UPDATE books SET name = ?, description = ?, image = ?, categoryid = ? WHERE bookId = ?",
+        [name, description, image, category, bookId]
+      );
+      res.redirect("/admin/books?action=updated");
+    } catch (err) {
+      console.log(err);
+      res.status(500).json({ error: "Internal Server Error" });
+    }
   }
-});
+);
 
-//Delete getting 
+//Delete getting
 router.get("/book/delete/:bookId", async (req, res) => {
   const bookId = req.params.bookId;
 
   try {
-    const [books, ] = await db.execute("SELECT * FROM books WHERE bookId = ?", [
+    const [books] = await db.execute("SELECT * FROM books WHERE bookId = ?", [
       bookId,
     ]);
     const book = books[0];
@@ -99,8 +116,7 @@ router.get("/book/delete/:bookId", async (req, res) => {
       title: "Delete Book",
       book: book,
     });
-  } 
-  catch (err) {
+  } catch (err) {
     console.log(err);
     res.status(500).json({ error: "GET DELETE ERROR" });
   }
@@ -113,8 +129,7 @@ router.post("/book/delete/:bookId", async (req, res) => {
   try {
     await db.execute("DELETE FROM books WHERE bookId = ?", [bookId]);
     res.redirect("/books?action=deleted");
-  } 
-  catch (err) {
+  } catch (err) {
     console.log(err);
     res.status(500).json({ error: "GET DELETE ERROR" });
   }
@@ -141,8 +156,8 @@ router.get("/books", async (req, res) => {
 router.get("/categories", async (req, res) => {
   console.log("Category");
   try {
-    const [categories, ] = await db.execute("SELECT * FROM category");
-    console.log("Category",categories);
+    const [categories] = await db.execute("SELECT * FROM category");
+    console.log("Category", categories);
     res.render("admin/category-list", {
       title: "Category List",
       categories: categories,
@@ -156,16 +171,20 @@ router.get("/categories", async (req, res) => {
 });
 
 //Create a category
-router.get("/category/create", async (req, res) => {
-  try {
-    res.render("admin/category-create", {
-      title: "Add Category",
-    });
-  } catch (err) {
-    console.log(err);
-    res.status(500).json({ error: "Internal Server Error" });
+router.get(
+  "/category/create",
+  imageUpload.upload.single("image"),
+  async (req, res) => {
+    try {
+      res.render("admin/category-create", {
+        title: "Add Category",
+      });
+    } catch (err) {
+      console.log(err);
+      res.status(500).json({ error: "Internal Server Error" });
+    }
   }
-});
+);
 
 //Creating a category
 router.post("/category/create", async (req, res) => {
@@ -183,9 +202,10 @@ router.post("/category/create", async (req, res) => {
 router.get("/categories/:categoryId", async (req, res) => {
   const categoryId = req.params.categoryId;
   try {
-    const [categories, ] = await db.execute("SELECT * FROM category WHERE categoryid = ?", [
-      categoryId,
-    ]);
+    const [categories] = await db.execute(
+      "SELECT * FROM category WHERE categoryid = ?",
+      [categoryId]
+    );
     const category = categories[0];
     if (category) {
       return res.render("admin/category-edit", {
@@ -205,10 +225,10 @@ router.post("/categories/:categoryId", async (req, res) => {
   const categoryId = req.params.categoryId;
   const name = req.body.name;
   try {
-    await db.execute("UPDATE category SET category_name = ? WHERE categoryid = ?", [
-      name,
-      categoryId,
-    ]);
+    await db.execute(
+      "UPDATE category SET category_name = ? WHERE categoryid = ?",
+      [name, categoryId]
+    );
     res.redirect("/admin/categories?action=updated");
   } catch (err) {
     console.log(err);
@@ -220,9 +240,10 @@ router.post("/categories/:categoryId", async (req, res) => {
 router.get("/category/delete/:categoryId", async (req, res) => {
   const categoryId = req.params.categoryId;
   try {
-    const [categories, ] = await db.execute("SELECT * FROM category WHERE categoryid = ?", [
-      categoryId,
-    ]);
+    const [categories] = await db.execute(
+      "SELECT * FROM category WHERE categoryid = ?",
+      [categoryId]
+    );
     const category = categories[0];
     res.render("admin/category-delete", {
       title: "Delete Category",
