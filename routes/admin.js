447ba8d2bccc59307fb7a97520d7a889930729e4
zahidsgn <zahide.dusgun@gmail.com>
2024-02-07 15:54:04 +0300

    Uptade function added to book table

--- routes/admin.js
+++ routes/admin.js
@@ -17,8 +17,8 @@ router.get("/book/create", async (req, res) => {
     res.status(500).json({ error: "Internal Server Error" });
   }
 });
+
 router.post("/book/create", async (req, res) => {
-  console.log("post içi");
   const name = req.body.name;
   const description = req.body.description;
   const image = req.body.image;
@@ -31,38 +31,13 @@ router.post("/book/create", async (req, res) => {
       "INSERT INTO books (name, description, image, categoryid, home, accept) VALUES (?, ?, ?, ?, ?, ?)",
       [name, description, image, category, home, accept]
     );
-    // res.redirect("/");
+     res.redirect("/");
   } catch (err) {
     console.log(err);
     res.status(500).json({ error: "Internal Server Error" });
   }
 });
-router.get("/book/:bookId", async (req, res) => {
-  const bookId = req.params.bookId;
-  try {
-    const [books] = await db.execute("SELECT * FROM books WHERE bookId = ?", [
-      bookId,
-    ]);
-    const [categories] = await db.execute("SELECT * FROM category");
-    const book = books[0];
-    console.log(book);
-    if (book) {
-      return (
-        res.render("admin/book-edit"),
-        {
-          name: book.name,
-          description: book.description,
-          book: book,
-          categories: categories,
-          console: console.log(categories),
-        }
-      );
-    }
-  } catch (err) {
-    console.log(err);
-    res.status(500).json({ error: "Internal Server Error" });
-  }
-});
+
 router.get("/books", async (req, res) => {
   try {
     const [books] = await db.execute("SELECT * FROM books");
@@ -70,6 +45,53 @@ router.get("/books", async (req, res) => {
       title: "Book List",
       books: books,
     });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+router.get("/books/:bookId", async (req, res) => {
+  const bookId = req.params.bookId;
+  try {
+    const [books, ] = await db.execute("SELECT * FROM books WHERE bookId = ?", [
+      bookId,
+    ]);
+    const [categories, ] = await db.execute("SELECT * FROM category");
+    const book = books[0];
+    console.log(book);
+    if (book) {
+      return res.render("admin/book-edit",
+        {
+          name: book.name,
+          description: book.description,
+          book: book,
+          image: book.image,
+          categories: categories,
+          console: console.log("burası",categories),
+        });
+    }
+    
+    res.redirect("admin/blogs");
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+router.post("/books/:bookId", async (req, res) => {
+  const bookId = req.params.bookId;
+  const name = req.body.name;
+  const description = req.body.description;
+  const image = req.body.image;
+  const category = req.body.category;
+
+  try {
+    await db.execute(
+      "UPDATE books SET name = ?, description = ?, image = ?, categoryid = ? WHERE bookId = ?",
+      [name, description, image, category, bookId]
+    );
+    res.redirect("/");
   } catch (err) {
     console.log(err);
     res.status(500).json({ error: "Internal Server Error" });
