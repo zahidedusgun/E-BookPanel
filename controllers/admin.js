83cb40aac78d541554e1b70c29510d80fcbc3ea5
zahidsgn <zahide.dusgun@gmail.com>
2024-02-11 15:55:55 +0300

    Admin routes seperated to controllers

---
+++ controllers/admin.js
@@ -0,0 +1,244 @@
+const Book = require("../models/book");
+const Category = require("../models/category");
+const fs = require("fs");
+
+exports.GetBookDelete = async (req, res) => {
+  const bookId = req.params.bookId;
+
+  try {
+    const books = await Book.findByPk(bookId);
+
+    if (books) {
+      return res.render("admin/book-delete", {
+        title: "Delete Book",
+        book: books.dataValues,
+      });
+    }
+    res.redirect("/admin/books");
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: "GET DELETE ERROR" });
+  }
+};
+
+exports.PostBookDelete = async (req, res) => {
+  const bookId = req.params.bookId;
+
+  try {
+    const book = await Book.findByPk(bookId);
+    if (book) {
+      await book.destroy();
+      return res.redirect("/admin/books?action=deleted");
+    }
+    res.redirect("/books?action=deleted");
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: "GET DELETE ERROR" });
+  }
+};
+
+exports.GetCategoryDelete = async (req, res) => {
+  const categoryId = req.params.categoryId;
+  try {
+    const category = await Category.findByPk(categoryId);
+    res.render("admin/category-delete", {
+      title: "Delete Category",
+      category: category,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+exports.PostCategoryDelete = async (req, res) => {
+  const categoryId = req.params.categoryId;
+  try {
+    await Category.destroy({
+      where: {
+        categoryId: categoryId,
+      },
+    });
+    res.redirect("/admin/categories?action=deleted");
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+exports.GetBookCreate = async (req, res) => {
+  try {
+    const categories = await Category.findAll();
+    res.render("admin/book-create", {
+      title: "Add Book",
+      categories: categories,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+exports.PostBookCreate = async (req, res) => {
+  const name = req.body.name;
+  const description = req.body.description;
+  const image = req.file.filename;
+  const category = req.body.category;
+
+  try {
+    await Book.create({
+      name: name,
+      description: description,
+      image: image,
+      categoryId: category,
+    });
+    res.redirect("/admin/books?action=created");
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+exports.GetCategoryCreate = async (req, res) => {
+  try {
+    res.render("admin/category-create", {
+      title: "Add Category",
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: "Category Create" });
+  }
+};
+
+exports.PostCategoryCreate = async (req, res) => {
+  const categoryName = req.body.categoryName;
+  try {
+    await Category.create({
+      categoryName: categoryName,
+    });
+    res.redirect("admin/categories?action=created");
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: "Category Create" });
+  }
+};
+
+exports.GetBookEdit = async (req, res) => {
+  const bookId = req.params.bookId;
+  try {
+    const book = await Book.findByPk(bookId);
+    const categories = await Category.findAll();
+
+    if (book) {
+      return res.render("admin/book-edit", {
+        book: book.dataValues,
+        categories: categories,
+      });
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+exports.PostBookEdit = async (req, res) => {
+  const bookId = req.params.bookId;
+  const name = req.body.name;
+  const description = req.body.description;
+  let image = req.body.image;
+  const category = req.body.category;
+
+  if (req.file) {
+    image = req.file.filename;
+    fs.unlink("./public/images/" + req.body.image, (err) => {
+      if (err) {
+        console.log(err);
+      }
+    });
+  }
+
+  try {
+    const book = await Book.findByPk(bookId);
+    if (book) {
+      book.name = name;
+      book.description = description;
+      book.image = image;
+      book.categoryId = category;
+      await book.save();
+      return res.redirect("/admin/books?action=updated");
+    }
+    res.redirect("/admin/books");
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+exports.GetCategoryEdit = async (req, res) => {
+  const categoryId = req.params.categoryId;
+  try {
+    const category = await Category.findByPk(categoryId);
+
+    if (category) {
+      return res.render("admin/category-edit", {
+        title: category.dataValues.categoryName,
+        category: category.dataValues,
+      });
+    }
+    res.redirect("/admin/categories");
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+exports.PostCategoryEdit = async (req, res) => {
+  const categoryId = req.params.categoryId;
+  const categoryName = req.body.categoryName;
+  try {
+    await Category.update(
+      { categoryName: categoryName },
+      {
+        where: {
+          categoryId: categoryId,
+        },
+      }
+    );
+    return res.redirect("/admin/categories?action=updated");
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+exports.GetAllBooks = async (req, res) => {
+  try {
+    const books = await Book.findAll({
+      attributes: ["bookId", "name", "description", "image"],
+    });
+    res.render("admin/book-list", {
+      title: "Book List",
+      books: books,
+      action: req.query.action,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+exports.GetAllCategories = async (req, res) => {
+    console.log("Category");
+    try {
+      const categories = await Category.findAll();
+      res.render("admin/category-list", {
+        title: "Category List",
+        categories: categories,
+        action: req.query.action,
+        categoryid: req.query.categoryid,
+      });
+    } catch (err) {
+      console.log(err);
+      res.status(500).json({ error: "Get Category Internal Server Error" });
+    }
+  }
--- routes/admin.js
+++ routes/admin.js
@@ -1,286 +1,59 @@
 const express = require("express");
 const router = express.Router();
-const path = require("path");
-const fs = require("fs");
 
-const db = require("../data/db");
 const imageUpload = require("../helpers/image-upload");
 
-
-const Book = require("../models/book");
-const Category = require("../models/category");
-
-
+const adminController = require("../controllers/admin");
 //Create a book
-router.get("/book/create", async (req, res) => {
-  try {
+router.get("/book/create", adminController.GetBookCreate);
 
-    const categories = await Category.findAll();
-    res.render("admin/book-create", {
-      title: "Add Book",
-      categories: categories,
-    });
-  }catch (err) {
-    console.log(err);
-    res.status(500).json({ error: "Internal Server Error" });
-  }
-});
-
-//Creating a book
-// Creating a book
 router.post(
   "/book/create",
   imageUpload.upload.single("image"),
-  async (req, res) => {
-    const name = req.body.name;
-    const description = req.body.description;
-    const image = req.file.filename;
-    const category = req.body.category;
- 
-    try {
-      await Book.create({
-        name: name,
-        description: description,
-        image: image,
-        categoryId: category,
-      });
-      res.redirect("/admin/books?action=created");
-    } catch (err) {
-      console.log(err);
-      res.status(500).json({ error: "Internal Server Error" });
-    }
-  }
+  adminController.PostBookCreate
 );
 
 //Selecting a book
-router.get("/books/:bookId", async (req, res) => {
-  const bookId = req.params.bookId;
-  try {
-    const book = await Book.findByPk(bookId);
-    const categories = await Category.findAll();
-
-    if (book) {
-      return res.render("admin/book-edit", {
-        book: book.dataValues,
-        categories: categories,
-      });
-    }
-  } catch (err) {
-    console.log(err);
-    res.status(500).json({ error: "Internal Server Error" });
-  }
-});
+router.get("/books/:bookId", adminController.GetBookEdit);
 
 //Editing the book
 router.post(
   "/books/:bookId",
   imageUpload.upload.single("image"),
-  async (req, res) => {
-    const bookId = req.params.bookId;
-    const name = req.body.name;
-    const description = req.body.description;
-    let image = req.body.image;
-    const category = req.body.category;
-
-    if (req.file) {
-      image = req.file.filename;
-      fs.unlink("./public/images/" + req.body.image, (err) => {
-        if (err) {
-          console.log(err);
-        }
-      });
-    }
-
-    try {
-      const book = await Book.findByPk(bookId);
-      if (book) {
-        book.name = name;
-        book.description = description;
-        book.image = image;
-        book.categoryId = category;
-        await book.save();
-        return res.redirect("/admin/books?action=updated");
-      }
-      res.redirect("/admin/books");
-    } catch (err) {
-      console.log(err);
-      res.status(500).json({ error: "Internal Server Error" });
-    }
-  }
+  adminController.PostBookEdit
 );
 
 //Delete getting
-router.get("/book/delete/:bookId", async (req, res) => {
-  const bookId = req.params.bookId;
-
-  try {
-    const books = await Book.findByPk(bookId);
-
-    if (books) {
-      return res.render("admin/book-delete", {
-        title: "Delete Book",
-        book: books.dataValues,
-      });
-    }
-    res.redirect("/admin/books");
-  } catch (err) {
-    console.log(err);
-    res.status(500).json({ error: "GET DELETE ERROR" });
-  }
-});
+router.get("/book/delete/:bookId", adminController.GetBookDelete);
 
 //Delete posting
-router.post("/book/delete/:bookId", async (req, res) => {
-  const bookId = req.params.bookId;
-
-  try {
-    const book = await Book.findByPk(bookId);
-    if (book) {
-      await book.destroy();
-      return res.redirect("/admin/books?action=deleted");
-    }
-    res.redirect("/books?action=deleted");
-  } catch (err) {
-    console.log(err);
-    res.status(500).json({ error: "GET DELETE ERROR" });
-  }
-});
+router.post("/book/delete/:bookId", adminController.PostBookDelete);
 
 //get posts
-router.get("/books", async (req, res) => {
-  try {
-    const books = await Book.findAll({
-      attributes: ["bookId", "name", "description", "image"],
-    });
-    res.render("admin/book-list", {
-      title: "Book List",
-      books: books,
-      action: req.query.action,
-    });
-  } catch (err) {
-    console.log(err);
-    res.status(500).json({ error: "Internal Server Error" });
-  }
-});
+router.get("/books", adminController.GetAllBooks);
 
 //Categories
 
 //get posts
-router.get("/categories", async (req, res) => {
-  console.log("Category");
-  try {
-    const categories = await Category.findAll();
-    res.render("admin/category-list", {
-      title: "Category List",
-      categories: categories,
-      action: req.query.action,
-      categoryid: req.query.categoryid,
-    });
-  } catch (err) {
-    console.log(err);
-    res.status(500).json({ error: "Get Category Internal Server Error" });
-  }
-});
+router.get("/categories", adminController.GetAllCategories);
 
 //Create a category
-router.get(
-  "/category/create",
-  imageUpload.upload.single("image"),
-  async (req, res) => {
-    try {
-      res.render("admin/category-create", {
-        title: "Add Category",
-      });
-    } catch (err) {
-      console.log(err);
-      res.status(500).json({ error: "Category Create" });
-    }
-  }
-);
+router.get("/category/create", adminController.GetCategoryCreate);
 
 //Creating a category
-router.post("/category/create", async (req, res) => {
-  const categoryName = req.body.categoryName;
-  try {
-    await Category.create({
-      categoryName: categoryName,
-    });
-    res.redirect("admin/categories?action=created");
-  } catch (err) {
-    console.log(err);
-    res.status(500).json({ error: "Category Create" });
-  }
-});
+router.post("/category/create", adminController.PostCategoryCreate);
 
 // Editing a category
-router.get("/categories/:categoryId", async (req, res) => {
-  const categoryId = req.params.categoryId;
-  try {
-    const category = await Category.findByPk(categoryId);
-
-    if (category) {
-      return res.render("admin/category-edit", {
-        title: category.dataValues.categoryName,
-        category: category.dataValues,
-      });
-    }
-    res.redirect("/admin/categories");
-  } catch (err) {
-    console.log(err);
-    res.status(500).json({ error: "Internal Server Error" });
-  }
-});
+router.get("/categories/:categoryId", adminController.GetCategoryEdit);
 
 //Editing the category
-router.post("/categories/:categoryId", async (req, res) => {
-  const categoryId = req.params.categoryId;
-  const categoryName = req.body.categoryName;
-  try {
-    await Category.update(
-      { categoryName: categoryName },
-      {
-        where: {
-          categoryId: categoryId,
-        },
-      }
-    );
-    return res.redirect("/admin/categories?action=updated");
-  } catch (err) {
-    console.log(err);
-    res.status(500).json({ error: "Internal Server Error" });
-  }
-});
+router.post("/categories/:categoryId", adminController.PostCategoryEdit);
 
 //Delete Category getting
-router.get("/category/delete/:categoryId", async (req, res) => {
-  const categoryId = req.params.categoryId;
-  try {
-    const category = await Category.findByPk(categoryId);
-    res.render("admin/category-delete", {
-      title: "Delete Category",
-      category: category,
-    });
-  } catch (err) {
-    console.log(err);
-    res.status(500).json({ error: "Internal Server Error" });
-  }
-});
+router.get("/category/delete/:categoryId", adminController.GetCategoryDelete);
 
 //Delete Category posting
-router.post("/category/delete/:categoryId", async (req, res) => {
-  const categoryId = req.params.categoryId;
-  try {
-    await Category.destroy({
-      where: {
-        categoryId: categoryId,
-      },
-    });
-    res.redirect("/admin/categories?action=deleted");
-  } catch (err) {
-    console.log(err);
-    res.status(500).json({ error: "Internal Server Error" });
-  }
-});
+router.post("/category/delete/:categoryId", adminController.PostCategoryDelete);
 module.exports = router;
 
 router;
